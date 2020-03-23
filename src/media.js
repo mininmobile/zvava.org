433d664aa57b5a33288d8567dd2455e6a105b4e9
not ervin <notminin@pm.me>
2020-03-23 22:10:54 +0000

    add preview list

--- src/media.js
+++ src/media.js
@@ -1,1 +1,31 @@
-// for media.html 'webapp'
+if (!URLSearchParams) {
+	throw Error("outdated browser, no URLSearchParams support");
+}
+
+let page = new URLSearchParams(location.search).get("page");
+
+if (page) {
+	let content = document.getElementById("content");
+	console.log(page);
+} else {
+	let preview = document.getElementById("media");
+
+	fetch(new Request("/src/data/media.json"))
+		.then((result) => result.json())
+		.then((data) => {
+			preview.innerHTML = "";
+
+			for (let i = 0; i < data.length; i++) {
+				setTimeout(() => new Promise((resolve, error) => {
+					let item = `<li>
+						<a href="media.html?page=${data[i].page}">
+							${data[i].title}
+						</a>
+					</li>`;
+				
+					preview.innerHTML += item + "\n";
+					resolve(item);
+				}), 0);
+			}
+		}).catch(console.error);
+}
