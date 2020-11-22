9365f7b0148c14878c4be95dff731486fb48a25d
not ervin <notminin@pm.me>
2020-11-22 02:05:06 +0000

    read templates to memory

--- make.js
+++ make.js
@@ -1,2 +1,36 @@
-// THIS IS A MAKE FILE
-// FUCK YOU
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+const fs = require("fs");
+
+let templates = {};
+
+fs.readdir("src", (error, temps) => {
+	if (error)
+		return console.error(error);
+
+	let x = 0;
+
+	temps.forEach(a => fs.readFile("src/" + a, "utf8", (err, data) => {
+		if (err)
+			throw console.error(err);
+
+		templates[a] = data;
+		x++;
+
+		if (x == temps.length)
+			run();
+	}));
+});
+
+function run() {
+	fs.readdir("articles", (error, articles) => {
+		if (error)
+			return console.error(error);
+
+		articles.forEach(a => fs.readFile("articles/" + a, "utf8", (err, data) => {
+			if (err)
+				return console.error(err);
+
+			// shit
+		}));
+	});
+}
