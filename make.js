0fdb8ba18bf69ac2c96da0edb143cbbbb92bd85e
not ervin <notminin@pm.me>
2020-11-23 02:36:16 +0000

    parse articles at face value

--- make.js
+++ make.js
@@ -1,8 +1,12 @@
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 const fs = require("fs");
 
+/**
+ * @type {Object.<string, string>}
+ */
 let templates = {};
 
+// read all of the templates
 fs.readdir("src", (error, temps) => {
 	if (error)
 		return console.error(error);
@@ -13,25 +17,33 @@ fs.readdir("src", (error, temps) => {
 		if (err)
 			throw console.error(err);
 
-		templates[a] = data;
-		x++;
+		templates[a.substring(9, a.length - 5)] = data;
 
+		// make sure you have all the templates collected before proceeding
+		x++;
 		if (x == temps.length)
-			run();
+			fetchArticles();
 	}));
 });
 
-function run() {
-	fs.readdir("articles", (error, articles) => {
+// read and parse all of the articles
+function fetchArticles() {
+	fs.readdir("articles", (error, _articles) => {
 		if (error)
 			return console.error(error);
 
-		articles.forEach(a => fs.readFile("articles/" + a, "utf8", (err, data) => {
+		let articles = {};
+
+		let x = 0;
+
+		_articles
+			.map(a => a.substring(0, a.length - 3)) // remove .md
+			.forEach(a => fs.readFile("articles/" + a + ".md", "utf8", (err, data) =>
+		{
 			if (err)
 				return console.error(err);
 
 			let metadata = {};
-			let text;
 
 			{ // extract meta
 				let mend = data.indexOf("\n\n#");
@@ -47,8 +59,75 @@ function run() {
 				let end = data.indexOf("\n", start);
 
 				metadata.title = data.substring(start, end);
-				text = data.substring(end + 2, data.length - 2).split("\n\n");
+				metadata.content = data.substring(end + 2, data.length - 2).split("\n\n");
 			}
+
+			articles[a] = metadata;
+
+			// make sure you have all the articles collected before proceeding
+			x++
+			if (x == _articles.length)
+				generateArticles(articles);
 		}));
 	});
+}
+
+// convert the articles into html
+function generateArticles(articles) {
+	/**
+	 * @type {Array.<RegExpExecArray>}
+	 */
+
+	Object.keys(articles).forEach((article) => {
+		let a = articles[article];
+			a.page = article;
+
+		let result = testTemplate(templates.article, a);
+		console.log(result)
+	});
+}
+
+// parse and return a finished page
+// it expects to be given an article as the context
+function testTemplate(_page, article) {
+	let expressions = [..._page.matchAll(/\{.+?}/g)];
+	let page = _page;
+
+	expressions.forEach((_e, i) => {
+		let e = _e.toString();
+		let result;
+
+		if (e[1] + e[2] + e[3] == "IF(") {
+			// 4 is the position of the opening bracket in "{IF(..."
+			let qEnd = e.indexOf(")", 4);
+			let q = e.substring(4, qEnd); // q(uestion)
+			let r = e.substring(qEnd + 1, e.length - 1); // r(esult)
+
+			if (test(q, article) != "") {
+				result = r; // todo: do some parsing
+			}
+		} else {
+			result = test(e, article);
+		}
+
+		page = page.replace(e, result || "");
+	});
+
+	return page;
+}
+
+// parse and return result of [custom templating language]
+// it expects to be given an article as the context
+function test(expression, article) {
+	let e = expression[0] == "{" ?
+		expression.substring(1, expression.length - 1) : expression;
+
+	switch (e) {
+		case "ARTICLE.URL.RAW": return article.rawurl;
+		case "ARTICLE.PARAGRAPHS": return "<p>" + article.content[0] + "</p>";
+
+		default: {
+			return eval(e.toLowerCase());
+		}
+	}
 }
