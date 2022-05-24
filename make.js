fbb2680f2f92b48c33bde4d288476a1f35e13c8d
sophie <zvava@disroot.org>
2022-05-24 18:04:40 +0100

    generate gemini files

--- make.js
+++ make.js
@@ -1,29 +1,35 @@
 const fs = require("fs");
 
 // this function
-//   -> fetch templates
-//   -> fetchBlog
+//   -> fetchTemplates
 //   -> fetchWiki
 //   -> generateGemini
-//     -> create cache of gemini templates
-//     -> write gemini output files
+//     create cache of gemini templates
+//     write gemini output files
 //   -> generateHTML
-//     -> translate all gemini files into html
+//     translate all gemini files into html
+//   -> generateASS
+//     generate a .ass feed for both gemini and web versions of the site
 
 /** @type {Object.<string, string>} */
 let templates = {};
-/** @type {Object.<string, string>} */
-let cache = {};
-let blog = {};
-let wiki = {};
+let pages = {};
 
 fs.stat("out", (err, stats) => {
 	if (err) {
 		console.log("\x1b[90m->\x1b[0m creating output directories...");
 		fs.mkdirSync("out");
+		// gemini output directory
 		fs.mkdirSync("out/gemini");
+		fs.mkdirSync("out/gemini/wiki");
+		// html output directory
 		fs.mkdirSync("out/www");
+		fs.mkdirSync("out/www/wiki");
 	}
+
+	console.log("\x1b[90m->\x1b[0m updating images...");
+	fs.cpSync("src/images/", "out/gemini/images/", { recursive: true });
+	fs.cpSync("src/images/", "out/www/images/", { recursive: true });
 
 	fetchTemplates();
 })
@@ -42,60 +48,21 @@ function fetchTemplates() {
 			// make sure you have all the templates collected before proceeding
 			x++;
 			if (x == temps.length)
-				fetchBlog();
-		}));
-	});
-}
-
-// read and parse all of the articles
-function fetchBlog() {
-	console.log("\x1b[90m->\x1b[0m parsing articles...");
-
-	fs.readdir("src/blog", (error, _blog) => {
-		if (error) return console.error(error);
-
-		let x = 0;
-		_blog.map(a => a.substring(0, a.length - 4)) // remove .gmi
-			.forEach(a => fs.readFile("src/blog/" + a + ".gmi", "utf8", (err, _data) => {
-			if (err) return console.error(err);
-			let data = _data.replace(/\r\n/gm, "\n");
-
-			let metadata = { page: a, content: data };
-			// extract title
-			metadata["title"] = data.substring(2, data.indexOf("\n"));
-			// extract metadata
-			let metaStart = data.indexOf("```") + 4;
-			let metaEnd = data.indexOf("```", metaStart) - 1;
-			let meta = data.substring(metaStart, metaEnd).split(/ +|\n+/);
-			// extract date (first element of metadata)
-			metadata["date"] = meta.shift();
-			// extract categories (the remaining elements, hide commas)
-			metadata["category"] = meta.map(x => x.replace(",", ""));
-
-			blog[a] = metadata;
-
-			// make sure you have all the articles collected before proceeding
-			x++
-			if (x == _blog.length) {
-				// sort by date
-				blog = Object.fromEntries(Object.entries(blog).sort(([,a],[,b]) => new Date(b.date) - new Date(a.date)));
-				// generate
 				fetchWiki();
-			}
 		}));
 	});
 }
 
-// read and parse all of the notes
+// read and parse all of the wiki pages
 function fetchWiki() {
-	console.log("\x1b[90m->\x1b[0m parsing notes...");
+	console.log("\x1b[90m->\x1b[0m gathering wiki pages...");
 
 	fs.readdir("src/wiki", (error, _wiki) => {
 		if (error) return console.error(error);
 
 		let x = 0;
 		_wiki.map(a => a.substring(0, a.length - 4)) // remove .gmi
-			.forEach(a => fs.readFile("src/wiki/" + a + ".gmi", "utf8", (err, _data) => {
+		.forEach(a => fs.readFile("src/wiki/" + a + ".gmi", "utf8", (err, _data) => {
 			if (err) return console.error(err);
 			let data = _data.replace(/\r\n/gm, "\n");
 
@@ -105,304 +72,91 @@ function fetchWiki() {
 			// extract metadata
 			let metaStart = data.indexOf("```") + 4;
 			let metaEnd = data.indexOf("```", metaStart) - 1;
-			let meta = data.substring(metaStart, metaEnd).split(/ +|\n+/);
-			// extract dates
-			metadata["created"] = meta[1];
-			metadata["modified"] = meta[3] || metadata["created"];
+			data.substring(metaStart, metaEnd).split(/\n+/).map(x => x.split(/ +/))
+			.forEach((x) => {
+				let property = x.shift();
+				metadata[property] = property == "category" ?
+					x.map(y => y.replace(",", "")) : x.join(" ");
+			});
+			// ensure there is a modified field
+			if (!metadata["modified"])
+				metadata["modified"] = metadata["created"];
 
-			wiki[a] = metadata;
+			pages[a] = metadata;
 
 			// make sure you have all the pages collected before proceeding
 			x++
 			if (x == _wiki.length) {
 				// sort by modified
-				wiki = Object.fromEntries(Object.entries(wiki).sort(([,a],[,b]) => new Date(b.modified) - new Date(a.modified)));
+				pages = Object.fromEntries(Object.entries(pages).sort(([,a],[,b]) => new Date(b.modified) - new Date(a.modified)));
 				// generate
-				console.log(wiki);
-				//generateArticles();
+				generateGemini();
 			}
 		}));
 	});
 }
 
 // convert the articles into html
-function generateArticles() {
-	console.log("\x1b[90m->\x1b[0m generating articles...");
+function generateGemini() {
+	console.log("\x1b[90m->\x1b[0m generating gemini site...");
+	// sanity
+	let _pages = Object.keys(pages);
+	let files = {};
 
-	let _articles = Object.keys(articles)
-	_articles.forEach((article, i) => {
-		let a = articles[article];
-		let result = testTemplate(templates["template_article.html"], a);
+	// generate index
+	let _wikiRecent = _pages.slice(0, 7).map(p => {
+		let page = pages[p];
+		return `=> /wiki/${p}.xyz [${page.modified}] ${page.title} [${page.category[0]}]`;
+	}).join("\n");
+	files["index"] = templates["index.gmi"].replace("{wiki_recent}", _wikiRecent);
 
-		fs.writeFile("out/media/" + article + ".html", result, () =>
+	// generate wiki index
+	let _wikiAll = _pages.map(p => {
+		let page = pages[p];
+		return `=> /wiki/${p}.xyz [${page.modified}] ${page.title} [${page.category.join(", ")}]`;
+	}).join("\n");
+	files["wiki/index"] = templates["wiki-index.gmi"].replace("{wiki_all}", _wikiAll);
+
+	// generate wiki pages
+	_pages.forEach(p => {
+		let page = pages[p];
+		files["wiki/" + p] = templates["wiki-page.gmi"].replace("{content}", page.content);
+	});
+
+	// write pages
+	let _files = Object.keys(files);
+	_files.forEach((f, i) => {
+		let content = files[f]
+			// add filenames to thumbnails
+			.replace(/(\w*)\.(png|jpg) (thumbnail|cover|image)/gi, "$1.$2 $3 ($1.$2)")
+			// replace ambiguous links
+			.replace(/\.xyz/g, ".gmi");
+
+		fs.writeFile("out/gemini/" + f + ".gmi", content, () =>
 			checkProgress(i));
 	});
 
 	function checkProgress(i) {
 		// update terminal readout
-		process.stdout.write(`\r\x1b[32m-->\x1b[0m created article ${i + 1}/${_articles.length}`);
+		process.stdout.write(`\r\x1b[32m-->\x1b[0m written page ${i + 1}/${_files.length}`);
 		// if all articles have been generated
-		if (i == _articles.length - 1) {
+		if (i == _files.length - 1) {
 			process.stdout.write("\n");
-			generateNotes();
-		}
-	}
-}
-
-// convert the notes into html
-function generateNotes() {
-	console.log("\x1b[90m->\x1b[0m generating notes...");
-
-	let _notes = Object.keys(notes)
-	_notes.forEach((note, i) => {
-		let a = notes[note];
-		let result = testTemplate(templates["template_note.html"], undefined, a);
-
-		fs.writeFile("out/notes/" + note + ".html", result, () =>
-			checkProgress(i));
-	});
-
-	function checkProgress(i) {
-		// update terminal readout
-		process.stdout.write(`\r\x1b[32m-->\x1b[0m created note ${i + 1}/${_notes.length}`);
-		// if all notes have been generated
-		if (i == _notes.length - 1) {
-			process.stdout.write("\n");
-			generatePreviews();
 			generateASS();
 		}
-	}
-}
-
-// generate preview on index.html and generate media.html listing
-function generatePreviews() {
-	console.log("\x1b[90m->\x1b[0m generating previews...");
-
-	{ // generate index.html
-		let result = testTemplate(templates["template_index.html"], articles, notes);
-
-		fs.writeFile("out/index.html", result, () =>
-			console.log("\x1b[32m-->\x1b[0m generated index.html"));
-	}
-
-	{ // generate media.html
-		let result = testTemplate(templates["template_media.html"], articles, notes);
-
-		fs.writeFile("out/media.html", result, () =>
-			console.log("\x1b[32m-->\x1b[0m generated media.html"));
 	}
 }
 
 function generateASS() {
 	console.log("\x1b[90m->\x1b[0m generating feed.ass...");
 
-	let assEntries = Object.keys(articles).map((_a, i) => {
-		let a = articles[_a]; let date = new Date(a.date);
-		let month = (date.getUTCMonth() + 1).toString(); month = month.length == 1 ? month = "0" + month : month;
-		let day = date.getUTCDate().toString(); day = day.length == 1 ? day = "0" + day : day;
-		return `${date.getUTCFullYear()}-${month}-${day}	https://zvava.org/media/${a.page}.html	${a.title}`;
-	});
+	let assEntries = Object.keys(pages).map((p, i) => {
+		let page = pages[p];
+		return `${page.modified.replace(/\//g, "-")}	gemini://zvava.org/wiki/${page.page}.gmi	${page.title}`;
+	}).join("\n");
 
-	fs.writeFile("out/feed.ass", assEntries.join("\n"), () =>
-		console.log("\x1b[32m-->\x1b[0m generated feed.ass"));
-}
-
-// parse and return a finished page
-// it expects to be given an article as the context
-function testTemplate(_page, article, note) {
-	let expressions = [..._page.matchAll(/\{.+?}/g)];
-	let page = _page;
-
-	expressions.forEach((_e, i) => {
-		let e = _e.toString();
-		let result;
-
-		if (e[1] + e[2] + e[3] == "IF(") {
-			// 4 is the position of the opening bracket in "{IF(..."
-			let qEnd = e.indexOf(")", 4);
-			let q = e.substring(4, qEnd); // q(uestion)
-			let r = e.substring(qEnd + 1, e.length - 1); // r(esult)
-
-			if (test(q, article, note)) {
-				if (r[0] == "[") {
-					result = testTemplate(templates[r.substring(1, r.length - 1)], article, note);
-				} else {
-					result = r;
-				}
-			}
-		} else {
-			result = test(e, article, note);
-		}
-
-		page = page.replace(e, result || "");
-	});
-
-	return page;
-}
-
-// add support for stuff like headers etc. not requiring me to write in ugly html
-/** @param {Array.<String>} paragraphs */
-function parseParagraphs(paragraphs) {
-	return paragraphs.map((paragraph) => {
-		let p = ""; // content of paragraph
-		let _p = paragraph; // content of paragraph after adding title to p
-		// check for header
-		if (paragraph.charAt(0) == "#") {
-			let end = paragraph.indexOf("\n"); if (end == -1) end = undefined;
-			let title = paragraph.substring(paragraph.indexOf(" ") + 1, end);
-			// check for other tag sizes
-			let tag = "h1";
-			if (paragraph.charAt(1) == "#") {
-				tag = "h2";
-				if (paragraph.charAt(2) == "#")
-					tag = "h3";
-			}
-			// add to paragraph
-			p += `<${tag}>${title}</${tag}>`;
-			_p = end > 0 ? paragraph.substring(end + 1) : "";
-		}
-		// if not just header, add content to paragraph
-		if (_p.length > 0)
-			p += "<p>" + _p + "</p>";
-
-		return p;
-	}).join("\n\t\t");
-}
-
-// parse and return result of [custom templating language]
-// it expects to be given an article as the context
-function test(expression, article, note) {
-	let e = expression[0] == "{" ?
-		expression.substring(1, expression.length - 1) : expression;
-
-	switch (e) {
-		case "ARTICLE.URL.DOMAIN": {
-			if (article.url.startsWith("/")) {
-				return article.url;
-			} else {
-				let path = article.url.split("/");
-				return path[2];
-			}
-		} break;
-		case "ARTICLE.URL.EXTERNAL": return !article.url.startsWith("/");
-		case "ARTICLE.PARAGRAPHS": return parseParagraphs(article.content);
-		case "NOTE.PARAGRAPHS": return parseParagraphs(note.content);
-
-		case "ARTICLES.LATEST": { // generate a listing of the first 5 articles
-			let articles = article; // for my own sanity
-			let result = "";
-
-			Object.keys(articles).slice(0, 5).forEach(a => {
-				let article = articles[a];
-
-				// open
-				result += "<li>";
-
-				// add first type in long-form
-				result += "[" + article.type.split(", ")[0] + "] ";
-
-				// add link
-				result += "<a href=\"media/" + article.page + ".html\">";
-				result += article.title;
-				result += "</a> ";
-
-				// add date
-				let d = new Date(article.date);
-				let month = d.getUTCMonth() + 1;
-					month = month.toString().length == 2 ? month : "0" + month;
-				let day = d.getUTCDate();
-					day = day.toString().length == 2 ? day : "0" + day;
-				result += "<span class=\"date\">";
-				result +=	d.getUTCFullYear() + "/" + month + "/" + day;
-				result += "</span>";
-
-				// close
-				result += "</li>\n";
-			});
-
-			return result;
-		} break;
-
-		case "NOTES.LATEST": { // generate a listing of the 5 last updated notes
-			let notes = note; // for my own sanity
-			let result = "";
-
-			Object.keys(notes).slice(0, 5).forEach(a => {
-				let note = notes[a];
-
-				// open
-				result += "<li>";
-
-				// add link
-				result += "<a href=\"notes/" + note.page + ".html\">";
-				result += note.page  == "index" ? "index" : note.title;
-				result += "</a> ";
-
-				// add date
-				let d = new Date(note.modified);
-				let month = d.getUTCMonth() + 1;
-					month = month.toString().length == 2 ? month : "0" + month;
-				let day = d.getUTCDate();
-					day = day.toString().length == 2 ? day : "0" + day;
-				result += "<span class=\"date\">";
-				result +=	d.getUTCFullYear() + "/" + month + "/" + day;
-				result += "</span>";
-
-				// close
-				result += "</li>\n";
-			});
-
-			return result;
-		} break;
-
-		case "ARTICLES.ALL": { // generate a list of all articles
-			let articles = article; // for my own sanity
-			let result = "";
-
-			Object.keys(articles).forEach(a => {
-				let article = articles[a];
-
-				// open
-				result += "<li>";
-
-				// add types
-				result += "[";
-				result += article.type
-					.split(", ")
-					.map(x => x[0])
-					.join(", ");
-				result += "] ";
-
-				// add link
-				result += "<a href=\"media/" + article.page + ".html\">";
-				result += article.title;
-				result += "</a> ";
-
-				// add date
-				let d = new Date(article.date);
-				let month = d.getUTCMonth() + 1;
-					month = month.toString().length == 2 ? month : "0" + month;
-					let day = d.getUTCDate();
-					day = day.toString().length == 2 ? day : "0" + day;
-				result += "<span class=\"date\">";
-				result +=	d.getUTCFullYear() + "/" + month + "/" + day;
-				result += "</span>";
-
-				// close
-				result += "</li>\n";
-			});
-
-			return result;
-		}
-
-		default: {
-			try {
-				return eval(e.toLowerCase());
-			} catch (error) {
-				console.log(`\x1b[91m->\x1b[0m error whilst executing [\x1b[91m${e}\x1b[0m]`);
-				console.error(error);
-				return error.toString();
-			}
-		}
-	}
+	fs.writeFile("out/gemini/feed.ass", assEntries, () =>
+		console.log("\x1b[32m-->\x1b[0m generated gemini feed.ass"));
+	fs.writeFile("out/www/feed.ass", assEntries.replace(/gemini:\/\//g, "https://").replace(/\.gmi\t/g, ".html "), () =>
+		console.log("\x1b[32m-->\x1b[0m generated html feed.ass"));
 }
