3021bb237a1dc95b29f071a11f42721dcedd9eda
sophie <zvava@disroot.org>
2022-05-29 13:38:57 +0100

    sigh...

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 @echo off
 echo -> running remote publish.sh
-ssh zvava@zvava.org '/home/zvava/zvava.org/publish.sh' > sshlog.txt
+ssh zvava@zvava.org "bash /home/zvava/zvava.org/publish.sh" > sshlog.txt
 type sshlog.txt
--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ cd /home/zvava/zvava.org
 echo '-> fetching latest updates'
 git pull
 echo '-> running generator script'
-node make
+/opt/node-v16.15.0-linux-x64/bin/node make
 echo '-> copying files'
 cp -r out/gemini/* /var/gemini/content/
 cp -r out/www/* /var/www/
