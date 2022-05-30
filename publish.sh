94a15d2d0d9c09c87637ce8680640d3da07dd5e3
sophie <zvava@disroot.org>
2022-05-30 12:31:27 +0100

    add chronic the hemphog saga

--- publish.sh
+++ publish.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-cd /home/zvava/zvava.org
-echo '-> fetching latest updates'
-git pull
-echo '-> running generator script'
-/opt/node-v16.15.0-linux-x64/bin/node make
-echo '-> copying files'
-cp -r out/gemini/* /var/gemini/content/
-cp -r out/www/* /var/www/
-echo '-> done!'
+LIVE=live-server-01
+host=$(hostname)
+if [ $host != $LIVE ]
+then
+	ssh zvava@zvava.org "bash /home/zvava/zvava.org/publish.sh"
+else
+	cd /home/zvava/zvava.org
+	echo '-> fetching latest updates'
+	git pull
+	echo '-> running generator script'
+	/opt/node-v16.15.0-linux-x64/bin/node make
+	echo '-> copying files'
+	cp -r out/gemini/* /var/gemini/content/
+	cp -r out/www/* /var/www/
+	echo '-> done!'
+fi
